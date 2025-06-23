c127fbf8699e349164e2deab3bbbab57c5bced60
Emre Yapıcı <emreeyapicii@gmail.com>
2025-06-23 21:13:48 +0300

    Karakter animasyonları eklendi + PlayerController güncellendi.
    Run animation + Idle animation + Hit animation eklendi.
    Player'a rigidbody eklendi.
    PlayerController güncellendi.

--- Assets/Gameplay Scripts/PlayerController.cs
+++ Assets/Gameplay Scripts/PlayerController.cs
@@ -3,35 +3,52 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float Speed = 5f;
     private Vector2 moveInput;
 
+    [Header("Hit Reaction Settings")]
+    private bool isHit = false;
+    private float hitTimer = 0f;
+
+    private Animator animator;
     private Camera mainCamera;
 
+    // Unity Event Methods
     private void Start()
     {
         mainCamera = Camera.main;
+        animator = GetComponentInChildren<Animator>();
     }
 
+    private void Update()
+    {
+        HandleHitTimer();
+        HandleMovement();
+        HandleRotation();
+        HandleHitTrigger();
+        UpdateAnimatorParameters();
+    }
+
+    // Input System Events
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
 
-    void Update()
+    // Movement & Rotation
+    private void HandleMovement()
     {
-        MovePlayer();
-        RotateToMouse();
-    }
+        if (isHit) return;
 
-    private void MovePlayer()
-    {
         Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
         transform.Translate(moveDirection * Speed * Time.deltaTime, Space.World);
     }
 
-    private void RotateToMouse()
+    private void HandleRotation()
     {
+        if (isHit) return;
+
         Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
 
@@ -46,6 +63,42 @@ public class PlayerController : MonoBehaviour
                 Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
                 transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 0.1f);
             }
+        }
+    }
+
+    // Hit Reaction
+    private void HandleHitTrigger()
+    {
+        if (Keyboard.current.hKey.wasPressedThisFrame && !isHit)
+        {
+            animator.SetTrigger("Hit");
+            isHit = true;
+            hitTimer = 1.4f; // Hit animasyon süresi
+        }
+    }
+
+    private void HandleHitTimer()
+    {
+        if (!isHit) return;
+
+        hitTimer -= Time.deltaTime;
+        if (hitTimer <= 0f)
+        {
+            isHit = false;
+        }
+    }
+
+    // Animator
+    private void UpdateAnimatorParameters()
+    {
+        if (isHit)
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+        else
+        {
+            float speed = moveInput.magnitude;
+            animator.SetFloat("Speed", speed);
         }
     }
 }
